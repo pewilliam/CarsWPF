bbc92b56468ca290018c4e7d8f90afed0aac0792
Pedro William 🎮🎶 <axlwilliamrose5@hotmail.com>
2023-06-14 08:13:07 -0300

    Added InsertBrandWindow in InsertCarWindow to allow the user to add new brands. Added functionality to check if fields have value before inserting in db.

--- CarsWPF/InsertCarWindow.xaml.cs
+++ CarsWPF/InsertCarWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text.RegularExpressions;
@@ -29,22 +30,66 @@ namespace CarsWPF
 
         private void btnSalvar_Click(object sender, RoutedEventArgs e)
         {
-            string namecar = txbName.Text;
-            string colorcar = txbColor.Text;
-            double pricecar = double.Parse(txbPrice.Text, NumberStyles.AllowCurrencySymbol | NumberStyles.Currency);
-            string yearcar = txbYear.Text;
-            string qtpassengers = txbQtde.Text;
-            int brand = 1;
-            if (cb.SelectedValue != null)
+            try
             {
-                brand = (int)cb.SelectedValue;
+                string namecar = txbName.Text;
+                string colorcar = txbColor.Text;
+                double pricecar = double.Parse(txbPrice.Text, NumberStyles.AllowCurrencySymbol | NumberStyles.Currency);
+                string yearcar = txbYear.Text;
+                string qtpassengers = txbQtde.Text;
+                int brand = 1;
+                if (cb.SelectedValue != null)
+                {
+                    brand = (int)cb.SelectedValue;
+                }
+
+                string sql = $"SELECT public.insertcar('{namecar}', '{colorcar}', {pricecar}, {yearcar}, {qtpassengers}, {brand})";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Inserido com sucesso!", "Concluído");
+                Close();
+            }
+            catch (Exception)
+            {
+                CheckFields();
+                MessageBox.Show("Não é possível inserir novo carro!\n Verifique o preenchimento dos dados.", "Erro");
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
             }
 
-            string sql = $"SELECT public.insertcar('{namecar}', '{colorcar}', {pricecar}, {yearcar}, {qtpassengers}, {brand})";
-            NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
-            cmd.ExecuteNonQuery();
-            MessageBox.Show("Inserido com sucesso!", "Concluído");
-            Close();
+            if (e.Key == Key.Enter)
+            {
+                try
+                {
+                    string namecar = txbName.Text;
+                    string colorcar = txbColor.Text;
+                    double pricecar = double.Parse(txbPrice.Text, NumberStyles.AllowCurrencySymbol | NumberStyles.Currency);
+                    string yearcar = txbYear.Text;
+                    string qtpassengers = txbQtde.Text;
+                    int brand = 1;
+                    if (cb.SelectedValue != null)
+                    {
+                        brand = (int)cb.SelectedValue;
+                    }
+
+                    string sql = $"SELECT public.insertcar('{namecar}', '{colorcar}', {pricecar}, {yearcar}, {qtpassengers}, {brand})";
+                    NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Inserido com sucesso!", "Concluído");
+                    Close();
+                }
+                catch (Exception)
+                {
+                    CheckFields();
+                    MessageBox.Show("Não é possível inserir novo carro!\nVerifique o preenchimento dos dados.", "Erro");
+                }
+            }
         }
 
         private void PopulateCB(object sender, System.EventArgs e)
@@ -96,49 +141,44 @@ namespace CarsWPF
             }
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Escape)
-            {
-                Close();
-            }
-
-            warningLabelName.Content = "";
-            warningLabelColor.Content = "";
-            warningLabelPrice.Content = "";
-            warningLabelYear.Content = "";
-            warningLabelQtde.Content = "";
-
-            if (e.Key == Key.Enter)
-            {
-                if (string.IsNullOrEmpty(txbName.Text))
-                {
-                    warningLabelName.Content = "Digite um nome!";
-                }
-                if (string.IsNullOrEmpty(txbColor.Text))
-                {
-                    warningLabelColor.Content = "Digite uma cor!";
-                }
-                if (string.IsNullOrEmpty(txbPrice.Text))
-                {
-                    warningLabelPrice.Content = "Digite um preço!";
-                }
-                if (string.IsNullOrEmpty(txbYear.Text))
-                {
-                    warningLabelYear.Content = "Digite um ano!";
-                }
-                if (string.IsNullOrEmpty(txbQtde.Text))
-                {
-                    warningLabelQtde.Content = "Digite uma quantidade!";
-                }
-            }
-        }
-
         private void insertBrand_Click(object sender, RoutedEventArgs e)
         {
             InsertBrandWindow insertBrandWindow = new InsertBrandWindow();
             insertBrandWindow.conn = conn;
             insertBrandWindow.ShowDialog();
+        }
+
+        private void CheckFields()
+        {
+            if (string.IsNullOrEmpty(txbName.Text))
+            {
+                warningLabelName.Content = "Digite um nome!";
+            }
+            if (string.IsNullOrEmpty(txbColor.Text))
+            {
+                warningLabelColor.Content = "Digite uma cor!";
+            }
+            if (string.IsNullOrEmpty(txbPrice.Text))
+            {
+                warningLabelPrice.Content = "Digite um preço!";
+            }
+            if (string.IsNullOrEmpty(txbYear.Text))
+            {
+                warningLabelYear.Content = "Digite um ano!";
+            }
+            if (string.IsNullOrEmpty(txbQtde.Text))
+            {
+                warningLabelQtde.Content = "Digite uma quantidade!";
+            }
+        }
+
+        private void txb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txbName.Text)) warningLabelName.Content = "";
+            if (!string.IsNullOrEmpty(txbColor.Text)) warningLabelColor.Content = "";
+            if (!string.IsNullOrEmpty(txbPrice.Text)) warningLabelPrice.Content = "";
+            if (!string.IsNullOrEmpty(txbYear.Text)) warningLabelYear.Content = "";
+            if (!string.IsNullOrEmpty(txbQtde.Text)) warningLabelQtde.Content = "";
         }
     }
 }
